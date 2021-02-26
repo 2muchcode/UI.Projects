c27363eae74df57901d15692b515eed63a72136b
propriotr <zxvf@tutanota.com>
2021-02-26 22:15:51 +0530

    updated

--- calcapp/script.js
+++ calcapp/script.js
@@ -2,4 +2,4 @@ const numpad = document.querySelectorAll(".numpad");
 const prevDisplay = document.querySelector(".prev-display");
 const mainDisplay = document.querySelector(".main-display");
 
-mainDisplay.innerText = "Yo, we married";
+
