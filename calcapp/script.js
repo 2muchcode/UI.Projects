649e9a69fecfd1400bf9192b9f53847de1abf262
propriotr <zxvf@tutanota.com>
2021-02-26 23:09:27 +0530

    changed styles

--- calcapp/script.js
+++ calcapp/script.js
@@ -1,5 +1,9 @@
-const numpad = document.querySelectorAll(".numpad");
-const prevDisplay = document.querySelector(".prev-display");
+const prevDisplay = document.querySelector(".prev-display").textContent;
 const mainDisplay = document.querySelector(".main-display");
+const numpad = document.querySelectorAll(".numpad");
+const operator = document.querySelector(".operator");
+const allClear = document.querySelector(".all-clear");
+const deleteButton = document.querySelector(".delete");
 
 
+console.log(numpad);
